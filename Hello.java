a744e1ac1ba38355468644746a3cf9ef55ecb204
Navaneeth Rayudu  Basala <navaneeth.002@gmail.com>
2022-03-11 23:11:46 +0530

    added a method

--- Hello.java
+++ Hello.java
@@ -2,4 +2,9 @@ class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
     }
+	
+	public static int add(int a, int b){
+	
+	return a+b;
+	}
 }
